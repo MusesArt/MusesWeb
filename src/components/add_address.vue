b4f5de669efe08910acdad39a35e3f50eff0fe53
Emotiongt <854797945@qq.com>
2018-12-31 18:29:25 +0800

    添加地址页面

---
+++ src/components/add_address.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<div class="head">
+			<img src="../assets/back2.png">
+			<p>添加收货地址</p>
+		</div>
+	</div>
+</template>
+<script>
+	
+</script>
+<style>
+p{
+	margin:0px;
+}
+hr{
+	margin-bottom:10px;
+}
+.head{
+    width:100%;
+	height:50px;
+	margin-bottom:5px;
+	background:white;
+	/*border:1px solid #aaa;*/
+}
+.head img{
+	margin:10px 0 0 10px;
+	width:30px;
+	height:30px;
+	float:left;
+}
+.head p{
+	text-align:center;
+	color:#333;
+	font-size:16px;
+	font-weight: bold;
+	line-height:50px;
+	margin-right:22px;
+}
+.container{
+	margin:0 10px;
+}
+</style>
